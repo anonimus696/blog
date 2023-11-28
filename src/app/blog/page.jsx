ea2fda7629101cee58c0d27d192ce67cc8bb1098
anonimus696 <korovinv311@gmail.com>
2023-11-28 18:01:16 +0200

    another update.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 
 async function getData() {
-    const res = await fetch('http://localhost:3000/api/posts', {
+    const res = await fetch(process.env.URL + '/api/posts', {
         cache: "no-store",
     })
 
